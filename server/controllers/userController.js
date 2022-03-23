0a008888c6f2d2e43378b22fc0fe1339cf512c2b
luke-buttifant <lukerhys.lb@gmail.com>
2022-03-23 00:16:24 +0000

    Encrpt password on post user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../model/userModel')
+const asyncHandler = require('express-async-handler');
 
 // Get users
 // Route: /api/users
@@ -9,7 +10,32 @@ const getUsers = async (req, res) => {
     res.status(200).json(users)
 }
 
+const registerUser = asyncHandler(async (req, res) => {
+    const {first_name, last_name, email, password, gender, is_admin} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if (userExists)
+    {
+    res.status(400);
+    throw new Error('User already exists with that email.')
+    }
+
+    const user = await User.create({first_name, last_name, email, password, gender, is_admin});
+
+    if(user){
+        res.status(201).json({
+            first_name:user.first_name,
+            last_name:user.last_name,
+            email:user.email,
+        })}
+        else{
+            res.status(400)
+            throw new Error("Error Occured!")
+        }
+});
 
 
 
-module.exports = { getUsers}
+
+module.exports = { getUsers, registerUser}
--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema(
   {
@@ -26,12 +27,18 @@ const userSchema = mongoose.Schema(
     },
     is_admin: {
         type: Boolean,
-        
     },
   },
   {
     timestamps: true,
   }
 )
+userSchema.pre('save', async function(next){
+  if(!this.isModified('password')){
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 module.exports = mongoose.model('User', userSchema)
