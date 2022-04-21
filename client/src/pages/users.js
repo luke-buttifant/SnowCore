c60c85655bd53611758fb17a87afed56a4a7ee27
luke-buttifant <lukerhys.lb@gmail.com>
2022-04-21 14:34:55 +0000

    view users

--- client/src/pages/users.js
+++ client/src/pages/users.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataGrid } from '@mui/x-data-grid';
+import AdminNav from "../components/adminNav";
 
 
 const Users = () =>{
@@ -10,7 +11,7 @@ const Users = () =>{
 
     useEffect(() => {
       userAuthenticated();
-      usersDataGrid();
+      // usersDataGrid();
       getUsersrData();
       }, [navigate]);
 
@@ -37,25 +38,64 @@ const Users = () =>{
       userData(response.data)
     })};
 
-      function usersDataGrid() {
-        const { data } = userData({
-          dataSet: 'users',
-          rowLength: 100000,
-          editable: true,
-        });
-      
-        return (
-          <div style={{ height: 520, width: '100%' }}>
-            <DataGrid
-              {...data}
-              loading={data.rows.length === 0}
-              rowHeight={38}
-              checkboxSelection
-              disableSelectionOnClick
-            />
-          </div>
-        );
-      }
+      // function usersDataGrid() {
+      //   const { data } = userData({
+      //     dataSet: 'users',
+      //     rowLength: 100000,
+      //     editable: true,
+      //   })};
+
+        const columns = [
+          { field: 'id', headerName: 'ID', width: 90 },
+          {
+            field: 'first_name',
+            headerName: 'First name',
+            width: 150,
+            editable: true,
+          },
+          {
+            field: 'last_name',
+            headerName: 'Last name',
+            width: 150,
+            editable: true,
+          },
+          {
+            field: 'email',
+            headerName: 'Email',
+            width: 110,
+            editable: true,
+          },
+          {
+            field: 'gender',
+            headerName: 'Gender',
+            width: 160,
+          },
+          {
+            field: 'dob',
+            headerName: 'Date of Birth',
+            width: 110,
+            editable: true,
+          },
+        ];
+      var rows = []
+        for(let i = 0; i < users.length; i++){
+          rows.push({id: i, first_name: users[i].first_name, last_name: users[i].last_name, email: users[i].email, gender: users[i].gender, dob: users[i].dob})
+        }
+return (
+  <>
+  <AdminNav />
+  <div className="bg-white mx-auto" style={{ height: 520, width: '80%' }}>
+    <DataGrid
+      columns={columns}
+      rows={rows}
+      loading={users.length === 0}
+      rowHeight={50}
+      checkboxSelection
+      disableSelectionOnClick
+    />
+  </div>
+  </>
+);
 
 
 };
