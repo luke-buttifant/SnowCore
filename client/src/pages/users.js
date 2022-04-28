f78f064f3eb3e3000d4d688db210ad815bf58911
unknown <3bilyi81@solent.ac.uk>
2022-04-28 13:14:37 +0100

    usersConfirmationSavingNotFinished

--- client/src/pages/users.js
+++ client/src/pages/users.js
@@ -4,6 +4,22 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataGrid } from '@mui/x-data-grid';
 import AdminNav from "../components/adminNav";
+//import { Button } from 'react-bootstrap';
+import {
+  randomCreatedDate,
+  randomTraderName,
+  randomUpdatedDate,
+} from '@mui/x-data-grid-generator';
+import Snackbar from '@mui/material/Snackbar';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
+
+
 
 
 const Users = () =>{
@@ -45,6 +61,115 @@ const Users = () =>{
       //     editable: true,
       //   })};
 
+      //Editing with confirmation
+      const useFakeMutation = () => {
+        return React.useCallback(
+          (user) =>
+            new Promise((resolve, reject) =>
+              setTimeout(() => {
+                if (user.name?.trim() === '') {
+                  reject();
+                } else {
+                  resolve(user);
+                }
+              }, 200),
+            ),
+          [],
+        );
+      };
+      
+      function computeMutation(newRow, oldRow) {
+        if (newRow.name !== oldRow.name) {
+          return `Name from '${oldRow.name}' to '${newRow.name}'`;
+        }
+        if (newRow.age !== oldRow.age) {
+          return `Age from '${oldRow.age || ''}' to '${newRow.age || ''}'`;
+        }
+        return null;
+      }
+
+      const mutateRow = useFakeMutation();
+  const noButtonRef = React.useRef(null);
+  const [promiseArguments, setPromiseArguments] = React.useState(null);
+
+  const [snackbar, setSnackbar] = React.useState(null);
+
+  const handleCloseSnackbar = () => setSnackbar(null);
+
+  const processRowUpdate = React.useCallback(
+    (newRow, oldRow) =>
+      new Promise((resolve, reject) => {
+        const mutation = computeMutation(newRow, oldRow);
+        if (mutation) {
+          // Save the arguments to resolve or reject the promise later
+          setPromiseArguments({ resolve, reject, newRow, oldRow });
+        } else {
+          resolve(oldRow); // Nothing was changed
+        }
+      }),
+    [],
+  );
+
+  const handleNo = () => {
+    const { oldRow, resolve } = promiseArguments;
+    resolve(oldRow); // Resolve with the old row to not update the internal state
+    setPromiseArguments(null);
+  };
+
+  const handleYes = async () => {
+    const { newRow, oldRow, reject, resolve } = promiseArguments;
+
+    try {
+      // Make the HTTP request to save in the backend
+      const response = await mutateRow(newRow);
+      setSnackbar({ children: 'User successfully saved', severity: 'success' });
+      resolve(response);
+      setPromiseArguments(null);
+    } catch (error) {
+      setSnackbar({ children: "Name can't be empty", severity: 'error' });
+      reject(oldRow);
+      setPromiseArguments(null);
+    }
+  };
+
+  const handleEntered = () => {
+    // The `autoFocus` is not used because, if used, the same Enter that saves
+    // the cell triggers "No". Instead, we manually focus the "No" button once
+    // the dialog is fully open.
+    // noButtonRef.current?.focus();
+  };
+
+  const renderConfirmDialog = () => {
+    if (!promiseArguments) {
+      return null;
+    }
+
+    const { newRow, oldRow } = promiseArguments;
+    const mutation = computeMutation(newRow, oldRow);
+    return (
+      <Dialog
+        maxWidth="xs"
+        TransitionProps={{ onEntered: handleEntered }}
+        open={!!promiseArguments}
+      >
+        <DialogTitle>Are you sure?</DialogTitle>
+        <DialogContent dividers>
+          {`Pressing 'Yes' will change ${mutation}.`}
+        </DialogContent>
+        <DialogActions>
+          <Button ref={noButtonRef} onClick={handleNo}>
+            No
+          </Button>
+          <Button onClick={handleYes}>Yes</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+
+
+
+      //Columns
         const columns = [
           { field: 'id', headerName: 'ID', width: 90 },
           {
@@ -52,6 +177,7 @@ const Users = () =>{
             headerName: 'First name',
             width: 150,
             editable: true,
+            
           },
           {
             field: 'last_name',
@@ -81,9 +207,13 @@ const Users = () =>{
         for(let i = 0; i < users.length; i++){
           rows.push({id: i, first_name: users[i].first_name, last_name: users[i].last_name, email: users[i].email, gender: users[i].gender, dob: users[i].dob})
         }
+
+        
 return (
+  renderConfirmDialog(),
   <>
   <AdminNav />
+  <div style={{ width: '100%' }}>
   <div className="bg-white mx-auto" style={{ height: 520, width: '80%' }}>
     <DataGrid
       columns={columns}
@@ -92,7 +222,18 @@ return (
       rowHeight={50}
       checkboxSelection
       disableSelectionOnClick
+      experimentalFeatures={{ newEditingApi: true }}
+      processRowUpdate={processRowUpdate}
+
+      
+      {...data}
     />
+    {!!snackbar && (
+        <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={6000}>
+          <Alert {...snackbar} onClose={handleCloseSnackbar} />
+        </Snackbar>
+      )}
+  </div>
   </div>
   </>
 );
